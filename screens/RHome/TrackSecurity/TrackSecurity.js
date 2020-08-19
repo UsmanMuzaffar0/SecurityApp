bc28e70da684d7d0fa5457ee584c29eeb271623d
Usman Muzaffar <umuzaffar46@gmail.com>
2020-08-19 10:25:40 +0500

    Old map Using Sliders

--- screens/RHome/TrackSecurity/TrackSecurity.js
+++ screens/RHome/TrackSecurity/TrackSecurity.js
@@ -1,102 +1,120 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import {
- 
   StyleSheet,
+  View,
   Text,
+  TouchableOpacity,
   Image,
+  Button,
   Alert,
- 
+  Platform,
+  Dimensions
 } from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker, Callout, Polygon, Circle} from 'react-native-maps';
+import MapView,
+{ PROVIDER_GOOGLE, Marker,Callout, Polygon, Circle }
+  from 'react-native-maps';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
 
 
+export default class TrackSecurity extends Component {
 
-export default class TrackSecurtity extends Component  {
   state={
-    coordinates: [
-      { name: '1', latitude: 37.8025259, longitude: -122.4351431 },
-      { name: '2', latitude: 37.7896386, longitude: -122.421646  },
-      { name: '3', latitude: 37.7665248, longitude: -122.4161628 },
-      { name: '4', latitude: 37.7734153, longitude: -122.4577787 },
-      { name: '5', latitude: 37.7948605, longitude: -122.4596065 },
-      { name: '6', latitude: 37.8025259, longitude: -122.4351431 }
-      
-    ]
+    focusedLocation:{
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta: 
+      Dimensions.get("window").width / 
+      Dimensions.get("window").height * 0.0122
+    },
+    locationChoosen: false
   }
 
-  showWelcomeMessage = () => {
-    Alert.alert(
-      'Welcome to Lahore Lahore e',
-      'Food is love',
-      [
-        {
-          text: 'Cancel',
-          style: ' cancel'
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    });
+    this.setState(prevState => {
+      return{
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          
         },
-        {
-          text: 'Ok'
-        }
-      ]
-    )
+        locationChoosen: true
+      };
+    });
+  };
+
+  getLocationHandler = () => {
+    Geolocation.getCurrentPosition(pos => {
+        const coordsEvent = {
+          nativeEvent: {
+            coordinate: {
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude
+            }
+          }
+        };
+        this.pickLocationHandler(coordsEvent);
+    }, err => {
+      console.log(err);
+      alert("Fetching the position failed, Please pick one manually!");
+    })
   }
 
-  render(){
-  return (
-    <MapView
-    provider = {PROVIDER_GOOGLE}
-    style={styles.map}
-    region = {{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.09,
-      longitudeDelta: 0.035
-    }}>
-      <Polygon
-      coordinates={this.state.coordinates}
-      fillColor={'rgba(100,100,200,0.2)'}
-      strokeWidth={3}>
-      
-      </Polygon>
-      <Circle
-        center={{latitude: 37.8025259, longitude: -122.4351431}}
-        radius={1000}
-        fillColor={'rgba(200,300,200,0.3)'}
-      />
-      <Marker 
-      draggable
-      coordinate ={{latitude: 37.7825259, longitude: -122.4351431}}>
+  render() {
+    let marker = null;
 
-        <Callout onPress={this.showWelcomeMessage}>
-          
-          <Image source={require('../../../images/sushi.png')} />
-          <Text>Lahore Lahore e</Text>
-          
-        </Callout>
-
-      </Marker>
-      {
-        this.state.coordinates.map(marker => (
-          <Marker
-          key={marker.name}
-          coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
-          title={marker.name}>
-
-            <Callout onPress={this.showWelcomeMessage}>
-          
-              <Image style={{width:50,height:50}} source={require('../../../images/sushi.png')} />
-              <Text>{marker.name}</Text>
-          
-            </Callout>
-          </Marker>
-        ))
-      }
-    </MapView>
-  );
-}
-}
-
-const styles= StyleSheet.create({
-  map:{
-    height: '100%'
+    if (this.state.locationChoosen){
+      marker = <Marker coordinate={this.state.focusedLocation}/>;
+    }
+    return (
+      <View style={styles.container}>
+        <MapView 
+        initialRegion={this.state.focusedLocation}
+        style={styles.map}
+        onPress={this.pickLocationHandler}
+        ref={ref => this.map= ref}
+        >
+        
+        {marker}
+        </MapView>
+        <TouchableOpacity style={styles.button} onPress={this.getLocationHandler}>
+          <Text style={styles.txt} >Locate Me</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
-})
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  button: {
+    backgroundColor:'#607d8b',
+        borderRadius: 25,
+        opacity:0.9,
+        width:100,
+        paddingVertical:12,
+        marginTop:30,
+        alignSelf:'center',
+        marginLeft:"65%"
+  },
+  txt: {
+    fontSize:16,
+        fontWeight:'500',
+        color:'#ffffff',
+        textAlign:'center',   
+  }
+ 
+});
