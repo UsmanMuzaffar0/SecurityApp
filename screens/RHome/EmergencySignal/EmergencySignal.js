ccf96abedd575a2872702e5221a6d1db9f51241b
Usman Muzaffar <umuzaffar46@gmail.com>
2020-08-24 18:42:35 +0500

    Implementation of Emergency Sinal flatlist..

--- screens/RHome/EmergencySignal/EmergencySignal.js
+++ screens/RHome/EmergencySignal/EmergencySignal.js
@@ -1,17 +1,75 @@
-import * as React from 'react';
-import { View, Text ,Button} from 'react-native';
+import React, {Component} from 'react';
+import { View, Text ,Button, ActivityIndicator, StyleSheet, FlatList} from 'react-native';
 
+export default class EmergencySignal extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null
+    }
+  }
 
-const  EmergencySignal =() => {
-  return (
-    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={{ marginTop:30 , padding: 30 }}>
-      <Button 
-            title="Emergecny Signal" 
-            color="#000000"
-            onPress={()=>alert("Emergency Signal sent to Security,Please Be patience.!!")}>
-      </Button>
-    </View>
-  );
+  componentDidMount() {
+      return fetch('https://api.thingspeak.com/channels/1121453/fields/2.json', )
+             .then ( ( response) => response.json())
+             .then( (responseJson) => {
+                
+              this.setState({
+                isLoading: false,
+                dataSource: responseJson.feeds,
+              })
+              
+             })
+             .catch((error) =>{
+               console.log(error);
+             });
+             
+  }
+  
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }else{
+      return(
+      <FlatList
+                    data={this.state.dataSource}
+                    keyExtractor={(item,index) => index.toString()}
+                    renderItem={({item}) =>
+                    <View style={{backgroundColor:'#546e7a',padding:10,margin:10}}>
+                        <Text style={{color:'#ffffff', fontWeight:'bold',}}>Entry ID: {item.entry_id}</Text>
+                        <Text style={{color:'#ffffff'}}>Smoke Sensing Value: {item.field1}</Text>
+                        <Text style={{color:'#ffffff'}}>Motion Sensor Value in Boolean form: {item.field2}</Text>
+                        <Text style={{color:'#ffffff'}}>Time of Detection:  {item.created_at}</Text>
+                    </View>
+                    } />
+      );
+
+      return(
+        <View style={styles.container}>
+          {feeds}
+          
+        </View>
+      );
+    }
+ 
 }
-export default EmergencySignal;
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center'
+    
+  },
+  item: {
+    flex: 1,
+
+  }
+})
